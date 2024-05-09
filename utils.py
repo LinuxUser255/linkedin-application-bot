dc2da571b304809d154c92a64bd973c5a1ae0b85
Chris <atnl@protonmail.com>
2024-05-09 18:36:26 -0400

    Update utils.py

--- utils.py
+++ utils.py
@@ -107,159 +107,158 @@ class LinkedinUrlGenerate:
                     path.append(url)
         return path
 
-    def checkJobLocation(self,job):
-        jobLoc = "&location=" +job
-        match job.casefold():
-            case "asia":
-                jobLoc += "&geoId=102393603"
-            case "europe":
-                jobLoc += "&geoId=100506914"
-            case "northamerica":
-                jobLoc += "&geoId=102221843&"
-            case "southamerica":
-                jobLoc +=  "&geoId=104514572"
-            case "australia":
-                jobLoc +=  "&geoId=101452733"
-            case "africa":
-                jobLoc += "&geoId=103537801"
+    def checkJobLocation(self, job):
+        jobLoc = "&location=" + job
+        if job.casefold() == "asia":
+            jobLoc += "&geoId=102393603"
+        elif job.casefold() == "europe":
+            jobLoc += "&geoId=100506914"
+        elif job.casefold() == "northamerica":
+            jobLoc += "&geoId=102221843"
+        elif job.casefold() == "southamerica":
+            jobLoc += "&geoId=104514572"
+        elif job.casefold() == "australia":
+            jobLoc += "&geoId=101452733"
+        elif job.casefold() == "africa":
+            jobLoc += "&geoId=103537801"
 
         return jobLoc
 
     def jobExp(self):
-        jobtExpArray = config.experienceLevels
-        firstJobExp = jobtExpArray[0]
+        jobExpArray = config.experienceLevels
+        firstJobExp = jobExpArray[0]
         jobExp = ""
-        match firstJobExp:
-            case "Internship":
-                jobExp = "&f_E=1"
-            case "Entry level":
-                jobExp = "&f_E=2"
-            case "Associate":
-                jobExp = "&f_E=3"
-            case "Mid-Senior level":
-                jobExp = "&f_E=4"
-            case "Director":
-                jobExp = "&f_E=5"
-            case "Executive":
-                jobExp = "&f_E=6"
-        for index in range (1,len(jobtExpArray)):
-            match jobtExpArray[index]:
-                case "Internship":
-                    jobExp += "%2C1"
-                case "Entry level":
-                    jobExp +="%2C2"
-                case "Associate":
-                    jobExp +="%2C3"
-                case "Mid-Senior level":
-                    jobExp += "%2C4"
-                case "Director":
-                    jobExp += "%2C5"
-                case "Executive":
-                    jobExp  +="%2C6"
+
+        if firstJobExp == "Internship":
+            jobExp = "&f_E=1"
+        elif firstJobExp == "Entry level":
+            jobExp = "&f_E=2"
+        elif firstJobExp == "Associate":
+            jobExp = "&f_E=3"
+        elif firstJobExp == "Mid-Senior level":
+            jobExp = "&f_E=4"
+        elif firstJobExp == "Director":
+            jobExp = "&f_E=5"
+        elif firstJobExp == "Executive":
+            jobExp = "&f_E=6"
+
+        for index in range(1, len(jobExpArray)):
+            if jobExpArray[index] == "Internship":
+                jobExp += "%2C1"
+            elif jobExpArray[index] == "Entry level":
+                jobExp += "%2C2"
+            elif jobExpArray[index] == "Associate":
+                jobExp += "%2C3"
+            elif jobExpArray[index] == "Mid-Senior level":
+                jobExp += "%2C4"
+            elif jobExpArray[index] == "Director":
+                jobExp += "%2C5"
+            elif jobExpArray[index] == "Executive":
+                jobExp += "%2C6"
 
         return jobExp
 
     def datePosted(self):
         datePosted = ""
-        match config.datePosted[0]:
-            case "Any Time":
-                datePosted = ""
-            case "Past Month":
-                datePosted = "&f_TPR=r2592000&"
-            case "Past Week":
-                datePosted = "&f_TPR=r604800&"
-            case "Past 24 hours":
-                datePosted = "&f_TPR=r86400&"
+        if config.datePosted[0] == "Any Time":
+            datePosted = ""
+        elif config.datePosted[0] == "Past Month":
+            datePosted = "&f_TPR=r2592000&"
+        elif config.datePosted[0] == "Past Week":
+            datePosted = "&f_TPR=r604800&"
+        elif config.datePosted[0] == "Past 24 hours":
+            datePosted = "&f_TPR=r86400&"
         return datePosted
 
     def jobType(self):
-        jobTypeArray = config.jobType
-        firstjobType = jobTypeArray[0]
-        jobType = ""
-        match firstjobType:
-            case "Full-time":
-                jobType = "&f_JT=F"
-            case "Part-time":
-                jobType = "&f_JT=P"
-            case "Contract":
-                jobType = "&f_JT=C"
-            case "Temporary":
-                jobType = "&f_JT=T"
-            case "Volunteer":
-                jobType = "&f_JT=V"
-            case "Intership":
-                jobType = "&f_JT=I"
-            case "Other":
-                jobType = "&f_JT=O"
-        for index in range (1,len(jobTypeArray)):
-            match jobTypeArray[index]:
-                case "Full-time":
-                    jobType += "%2CF"
-                case "Part-time":
-                    jobType +="%2CP"
-                case "Contract":
-                    jobType +="%2CC"
-                case "Temporary":
-                    jobType += "%2CT"
-                case "Volunteer":
-                    jobType += "%2CV"
-                case "Intership":
-                    jobType  +="%2CI"
-                case "Other":
-                    jobType  +="%2CO"
-        jobType += "&"
-        return jobType
+       """jobTypeArray = config.jobType"""
+       jobTypeArray = config.jobType
+       firstjobType = jobTypeArray[0]
+       jobType = ""
+       if firstjobType == "Full-time":
+           jobType = "&f_JT=F"
+       elif firstjobType == "Part-time":
+           jobType = "&f_JT=P"
+       elif firstjobType == "Contract":
+           jobType = "&f_JT=C"
+       elif firstjobType == "Temporary":
+           jobType = "&f_JT=T"
+       elif firstjobType == "Volunteer":
+           jobType = "&f_JT=V"
+       elif firstjobType == "Intership":
+           jobType = "&f_JT=I"
+       elif firstjobType == "Other":
+           jobType = "&f_JT=O"
+
+       for index in range(1, len(jobTypeArray)):
+           if jobTypeArray[index] == "Full-time":
+               jobType += "%2CF"
+           elif jobTypeArray[index] == "Part-time":
+               jobType += "%2CP"
+           elif jobTypeArray[index] == "Contract":
+               jobType += "%2CC"
+           elif jobTypeArray[index] == "Temporary":
+               jobType += "%2CT"
+           elif jobTypeArray[index] == "Volunteer":
+               jobType += "%2CV"
+           elif jobTypeArray[index] == "Intership":
+               jobType += "%2CI"
+           elif jobTypeArray[index] == "Other":
+               jobType += "%2CO"
+
+       jobType += "&"
+       return jobType
 
     def remote(self):
+        """replacing the match statement with if else statement"""
         remoteArray = config.remote
         firstJobRemote = remoteArray[0]
         jobRemote = ""
-        match firstJobRemote:
-            case "On-site":
-                jobRemote = "f_WT=1"
-            case "Remote":
-                jobRemote = "f_WT=2"
-            case "Hybrid":
-                jobRemote = "f_WT=3"
-        for index in range (1,len(remoteArray)):
-            match remoteArray[index]:
-                case "On-site":
-                    jobRemote += "%2C1"
-                case "Remote":
-                    jobRemote += "%2C2"
-                case "Hybrid":
-                    jobRemote += "%2C3"
+        if firstJobRemote == "On-site":
+            jobRemote = "f_WT=1"
+        elif firstJobRemote == "Remote":
+            jobRemote = "f_WT=2"
+        elif firstJobRemote == "Hybrid":
+            jobRemote = "f_WT=3"
+
+        for index in range(1, len(remoteArray)):
+            if remoteArray[index] == "On-site":
+                jobRemote += "%2C1"
+            elif remoteArray[index] == "Remote":
+                jobRemote += "%2C2"
+            elif remoteArray[index] == "Hybrid":
+                jobRemote += "%2C3"
 
         return jobRemote
 
     def salary(self):
         salary = ""
-        match config.salary[0]:
-            case "$40,000+":
-                salary = "f_SB2=1&"
-            case "$60,000+":
-                salary = "f_SB2=2&"
-            case "$80,000+":
-                salary = "f_SB2=3&"
-            case "$100,000+":
-                salary = "f_SB2=4&"
-            case "$120,000+":
-                salary = "f_SB2=5&"
-            case "$140,000+":
-                salary = "f_SB2=6&"
-            case "$160,000+":
-                salary = "f_SB2=7&"    
-            case "$180,000+":
-                salary = "f_SB2=8&"    
-            case "$200,000+":
-                salary = "f_SB2=9&"                  
+        if config.salary[0] == "$40,000+":
+            salary = "f_SB2=1&"
+        elif config.salary[0] == "$60,000+":
+            salary = "f_SB2=2&"
+        elif config.salary[0] == "$80,000+":
+            salary = "f_SB2=3&"
+        elif config.salary[0] == "$100,000+":
+            salary = "f_SB2=4&"
+        elif config.salary[0] == "$120,000+":
+            salary = "f_SB2=5&"
+        elif config.salary[0] == "$140,000+":
+            salary = "f_SB2=6&"
+        elif config.salary[0] == "$160,000+":
+            salary = "f_SB2=7&"
+        elif config.salary[0] == "$180,000+":
+            salary = "f_SB2=8&"
+        elif config.salary[0] == "$200,000+":
+            salary = "f_SB2=9&"
+
         return salary
 
     def sortBy(self):
         sortBy = ""
-        match config.sort[0]:
-            case "Recent":
-                sortBy = "sortBy=DD"
-            case "Relevent":
-                sortBy = "sortBy=R"                
+        if config.sort[0] == "Recent":
+            sortBy = "sortBy=DD"
+        elif config.sort[0] == "Relevent":
+            sortBy = "sortBy=R"
         return sortBy
+
