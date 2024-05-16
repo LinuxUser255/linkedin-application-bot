659f835beebdc6292f41560a1fd36ee14b0a8660
Chris <atnl@protonmail.com>
2024-05-16 18:07:02 -0400

    Update test.py

--- test.py
+++ test.py
@@ -1,52 +1,56 @@
-import os,time,sys
+#!/usr/bin/env python3
 
+import sys
+import time
 from selenium import webdriver
 from selenium.webdriver.firefox.options import Options
+from utils import pr_green, pr_red, pr_yellow
 
-from utils import prGreen,prRed,prYellow
+pr_yellow("ℹ️  This script will check if the bot can automatically log in Linkedin for you.")
 
 
-prYellow("ℹ️  This script will check if the bot can automatically log in Linkedin for you.")
-
-def checkPython():
+def check_python():
     try:
-        if(sys.version):
-            prGreen("✅ Python is succesfully installed!")
+        if sys.version:
+            pr_green("✅ Python is succesfully installed!")
         else:
-            prRed("❌ Python is not installed please install Python first: https://www.python.org/downloads/")
+            pr_red("❌ Python is not installed please install Python first: https://www.python.org/downloads/")
     except Exception as e:
-        prRed(e)
+        pr_red(e)
 
-def checkPip():
+
+def check_pip():
     try:
         import pip
-        prGreen("✅ Pip is succesfully installed!")
+        pr_green("✅ Pip is succesfully installed!")
     except ImportError:
-        prRed("❌ Pip not present. Install pip: https://pip.pypa.io/en/stable/installation/")
+        pr_red("❌ Pip not present. Install pip: https://pip.pypa.io/en/stable/installation/")
 
-def checkSelenium():
+
+def check_selenium():
     try:
         import selenium
-        prGreen("✅ Selenium is succesfully installed!")
+        pr_green("✅ Selenium is succesfully installed!")
     except ImportError:
-        prRed("❌ Selenium not present. Install Selenium: https://pypi.org/project/selenium/")
+        pr_red("❌ Selenium not present. Install Selenium: https://pypi.org/project/selenium/")
 
-def checkFirefox():
+
+def check_firefox():
     try:
         import subprocess
         output = subprocess.check_output(['firefox', '--version'])
-        if(output):
-            prGreen("✅ Firefox is succesfully installed!")
+        if output:
+            pr_green("✅ Firefox is succesfully installed!")
         else:
-            prRed("❌ Firefox not present. Install firefox: https://www.mozilla.org/en-US/firefox/")
+            pr_red("❌ Firefox not present. Install firefox: https://www.mozilla.org/en-US/firefox/")
 
     except ImportError as e:
-        prRed(e)
+        pr_red(e)
 
-def checkSeleniumLinkedin():
 
+def check_selenium_linkedin():
     options = Options()
-    #firefoxProfileRootDir = os.getenv('firefoxProfileRootDir')
+    # firefoxProfileRootDir = os.getenv('firefoxProfileRootDir')
 
     options.add_argument("--start-maximized")
     options.add_argument("--ignore-certificate-errors")
@@ -54,37 +58,41 @@ def checkSeleniumLinkedin():
     options.add_argument("--disable-extensions")
     options.add_argument("--disable-blink-features")
     options.add_argument("--disable-blink-features=AutomationControlled")
-    #options.add_argument("-profile")
-    #options.add_argument(firefoxProfileRootDir)
-    #options.headless = True
+    # options.add_argument("-profile")
+    # options.add_argument(firefoxProfileRootDir)
+    # options.headless = True
 
     browser = webdriver.Firefox(options=options)
 
     try:
         browser.get('https://www.ongundemirag.com')
-        if(browser.title.index("Ongun")>-1):
-            prGreen("✅ Selenium and geckodriver is working succesfully!")
+        if browser.title.index("Ongun") > -1:
+            pr_green("✅ Selenium and geckodriver is working succesfully!")
         else:
-            prRed("❌ Please check if Selenium and geckodriver is installed")
+            pr_red("❌ Please check if Selenium and geckodriver is installed")
     except Exception as e:
-        prRed(e)
+        pr_red(e)
 
     try:
         browser.get('https://www.linkedin.com/feed/')
         time.sleep(3)
         if "Feed" in browser.title:
-            prGreen('✅ Successfully you are logged in to Linkedin, you can now run main bot script!')
+            pr_green('✅ Successfully you are logged in to Linkedin, you can now run main bot script!')
         else:
-            prRed('❌ You are not automatically logged in, please set up your Firefox Account correctly.')
+            pr_red('❌ You are not automatically logged in, please set up your Firefox Account correctly.')
     except Exception as e:
-        prRed(e)
+        pr_red(e)
     finally:
         browser.quit()
 
 
+def main():
+    check_python()
+    check_pip()
+    check_selenium()
+    check_firefox()
+    check_selenium_linkedin()
 
-checkPython()
-checkPip()
-checkSelenium()
-checkFirefox()
-checkSeleniumLinkedin()
+
+if __name__ == "__main__":
+    main()
